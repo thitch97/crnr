0b6793fb240743860565fdda3321485f32ee430a
thitch97 <th.einstein97@gmail.com>
2017-07-03 13:58:05 -0700

    Resolve merge conflicts

--- angular-client/src/app/search/search.component.ts
+++ angular-client/src/app/search/search.component.ts
@@ -1,28 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { SearchService } from '../search.service'
-=======
-import {SearchService} from '../search.service';
-
->>>>>>> 7fe01aa6fd9f32e64cdbda237303ea3827abbb90
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-<<<<<<< HEAD
 
-export class SearchComponent {
-  term: string;
-  result: string;
-=======
 export class SearchComponent {
 
   term: string;
   result: string;
   results = {};
->>>>>>> 7fe01aa6fd9f32e64cdbda237303ea3827abbb90
 
   constructor(private searchService: SearchService) {}
 
@@ -30,15 +19,10 @@ export class SearchComponent {
     this.searchService.search(this.term)
       .subscribe(data => {
         this.result = JSON.stringify(data);
-<<<<<<< HEAD
-      });
-  }
-
-=======
         this.results = data;
       });
   }
 
 
->>>>>>> 7fe01aa6fd9f32e64cdbda237303ea3827abbb90
+
 }
